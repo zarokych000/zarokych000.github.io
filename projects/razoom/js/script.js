b5bd918b1544093362f112ea7c3d446fb853a5c0
Ruslan <rusvolk97i@gmail.com>
2022-01-21 23:00:20 +0300

    Updates

---
+++ projects/razoom/js/script.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",()=>{$(".team-slider").slick({slidesToShow:1,centerMode:!0,centerPadding:"353px",prevArrow:".our-team__slider-btn--prev",nextArrow:".our-team__slider-btn--next"})});
+//# sourceMappingURL=script.js.map
